902371f210adee355da87131b891997b8536dd14
Ahmad Alyaaqba <ahmad.y91@hotmail.com>
2018-02-28 09:19:32 +0200

    ToyProblem week4#3

--- swap.js
+++ swap.js
@@ -3,4 +3,28 @@
 	// ToYPRoblEm --> tOyprOBLeM
 	function swap(input){
 		//your code is here
+		var result = ""
+		each(input,function(elem,i){
+			if (elem.toLowerCase() === elem){
+				result+= elem.toUpperCase();
+			}
+			else {
+				result+= elem.toLowerCase();
+			}
+		})
+		return result;
 	}
+
+	//helper Function
+	function each(coll, func) {
+    if (Array.isArray(coll)) {
+        for (var i = 0; i < coll.length; i++) {
+            func(coll[i], i);
+        }
+    }
+    else {
+        for (var key in coll) {
+            func(coll[key], key);
+        }
+    }
+}
